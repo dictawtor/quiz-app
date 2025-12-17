cabc4485f2bf994f9e6701adff257cdae673d034
morteza azami <71493723+dictawtor@users.noreply.github.com>
2025-12-17 15:18:16 +0330

    level git

--- js/level.js
+++ js/level.js
@@ -2,7 +2,7 @@ const levels = document.querySelectorAll('a');
 const selectHandler = (e) => {
     const level = e.target.innerText.toLowerCase();
     localStorage.setItem('level', level);
-    window.location.assign(" /index.html");
+    window.location.assign(" ./index.html");
 }   
 
 levels.forEach((level) => {
