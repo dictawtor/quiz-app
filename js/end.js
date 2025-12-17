8d411a2c6dfd98381a76e94fc0d1daa602c0fd37
morteza azami <71493723+dictawtor@users.noreply.github.com>
2025-12-17 15:17:15 +0330

    git

--- js/end.js
+++ js/end.js
@@ -20,7 +20,7 @@ if (!input.value || !score) {
     highScores.splice(10)
     localStorage.setItem("highScores" , JSON.stringify(highScores) )
     localStorage.removeItem("scores")
-    window.location.assign("scores.html")
+    window.location.assign("./scores.html")
 }
 }
 
